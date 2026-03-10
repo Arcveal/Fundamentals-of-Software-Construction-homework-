0dd2245f2b4e41646bd864762a4dcbbd605c9b42
Arcveal <yelaoshi0520@qq.com>
2026-03-10 12:00:31 +0800

    Add files via upload
    第一周作业

---
+++ 软件构造基础作业/第一周作业/Program1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你好！我是漆君淏");
+
+            Console.ReadKey();
+        }
+    }
+}
